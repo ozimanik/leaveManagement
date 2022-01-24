88d6cceb4904ab099624a20a8ce4e3380d54f6d4
Ozan Imik <O.Imik@adrom.at>
2022-01-24 12:04:20 +0100

    Setup User Auth, Default roles and admin, modified registration process

---
+++ leaveManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using leaveManagement.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(leaveManagement.Areas.Identity.IdentityHostingStartup))]
+namespace leaveManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- leaveManagement/Controllers/LeaveTypesController.cs
+++ leaveManagement/Controllers/LeaveTypesController.cs
@@ -2,6 +2,7 @@
 using leaveManagement.Contracts;
 using leaveManagement.Data;
 using leaveManagement.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace leaveManagement.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class LeaveTypesController : Controller
     {
 
